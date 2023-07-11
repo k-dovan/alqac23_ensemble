88b16f03d5a3ff382cc76741b0df1588d09ac6b8
k-dovan <khanhdovanit@gmail.com>
2023-07-11 16:40:20 +0700

    preprocessing corpus, train bm25plus

--- create_corpus.py
+++ create_corpus.py
@@ -4,71 +4,88 @@ import re
 from tqdm import tqdm
 import argparse
 
-def load_json(corpus_path):
-    data = json.load(open(corpus_path))
-    return data["items"]
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", default="./data", type=str, help="path to training data")
-    parser.add_argument("--save_dir", default="./generated_data", type=str, help="path to training data")
+    parser.add_argument("--data_dir", default="alqac23_data", type=str, help="path to training data")
+    parser.add_argument("--save_dir", default="generated_data", type=str, help="path to training data")
     args = parser.parse_args()
     os.makedirs(args.save_dir,exist_ok=True)
-    cp = open(os.path.join(args.save_dir, "corpus.txt"), "w")
-    corpus_path = os.path.join(args.data_dir, "legal_corpus.json")
 
-    data = json.load(open(corpus_path))
+    cp = open(os.path.join(args.save_dir, "corpus.txt"), "w")
+    alqac23_corpus_path = os.path.join(args.data_dir, "law.json")
+    alqac22_corpus_path = os.path.join(args.data_dir, "additional_data/ALQAC_2022_training_data/law.json")
+    zalo_corpus_path = os.path.join(args.data_dir, "additional_data/zalo/zalo_corpus.json")
+        
+    corpus_paths = [
+        alqac23_corpus_path,
+        alqac22_corpus_path,
+        zalo_corpus_path
+    ]
+    
+    data = []
+    for corpus_path in corpus_paths:
+        data.extend(json.load(open(corpus_path)))
 
     save_dict = {}
     co_f = open(os.path.join(args.save_dir, "cocondenser_data.json"), "w")
     count = 0
     for law_article in tqdm(data):
-        law_id = law_article["law_id"]
+        law_id = law_article["id"]
         law_articles = law_article["articles"]
         
         for sub_article in law_articles:
-            article_id = sub_article["article_id"]
-            article_title = sub_article["title"]
+            article_id = sub_article["id"]
             article_text = sub_article["text"]
-            article_full = article_title + ". " + article_text
-            article_full = article_full.replace("\n", " ")
+            article_full = article_text.replace("\n", " ")
             cp.write(article_full + "\n")
             
             # Save data for cocondenser 
-            spans = [article_title]
+            spans = []
             passages = re.split(r"\n[0-9]+\. |1\. ", article_text)
             for idx, p in enumerate(passages):
                 if p != "":
-                    article_full = article_title + ". " + p
-                    article_full = article_full.replace("\n", " ")
                     spans.append(p)
             co_f.write("#".join(spans) + "\n")
             
             concat_id = law_id + "_" + article_id
             if concat_id not in save_dict:
                 count += 1
-                save_dict[concat_id] = {"title": article_title, "text": article_text}
+                save_dict[concat_id] = {"text": article_text}
     
     co_f.close()
     print(count)
-    # exit()
+        
     print("Create legal dict from raw data")
     with open(os.path.join(args.save_dir, "legal_dict.json"), "w") as outfile:
         json.dump(save_dict, outfile)
     print("Finish")
-    corpus_path_train = os.path.join(args.data_dir, "train_question_answer.json")
-    items = load_json(corpus_path_train)
 
-    for item in tqdm(items):
-        question = item["question"]
+    # enrich corpus from train files
+    alqac23_corpus_path_train = os.path.join(args.data_dir, "train.json")
+    alqac22_corpus_path_train = os.path.join(args.data_dir, "additional_data/ALQAC_2022_training_data/question.json")
+    zalo_corpus_path_train = os.path.join(args.data_dir, "additional_data/zalo/zalo_question.json")
+     
+    train_corpus_paths = [
+        alqac23_corpus_path_train,
+        alqac22_corpus_path_train,
+        zalo_corpus_path_train
+    ]
+    
+    train_items = []
+    for train_corpus_path in train_corpus_paths:
+        train_items.extend(json.load(open(train_corpus_path)))
+
+    for item in tqdm(train_items):
+        question = item["text"]
         cp.write(question + "\n")
 
-    corpus_path_test = os.path.join(args.data_dir, "public_test_question.json")
-    items = load_json(corpus_path_test)
+    # enrich corpus from public test file
+    alqac23_corpus_path_test = os.path.join(args.data_dir, "public_test.json")
+    public_items = json.load(open(alqac23_corpus_path_test))
 
-    for item in tqdm(items):
-        question = item["question"]
+    for item in tqdm(public_items):
+        question = item["text"]
         cp.write(question + "\n")
 
     cp.close()
--- preprocessing.py
+++ preprocessing.py
@@ -8,25 +8,36 @@ import argparse
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--raw_data", default="zac2021-ltr-data", type=str, help="path to raw data")
+    parser.add_argument("--raw_data", default="alqac23_data", type=str, help="path to raw data")
     parser.add_argument("--save_path", default="generated_data", type=str, help="path to save doc refer.")
     args = parser.parse_args()
 
-    data = json.load(open(os.path.join(args.raw_data, "legal_corpus.json")))
+    alqac23_corpus_path = os.path.join(args.raw_data, "law.json")
+    alqac22_corpus_path = os.path.join(args.raw_data, "additional_data/ALQAC_2022_training_data/law.json")
+    zalo_corpus_path = os.path.join(args.raw_data, "additional_data/zalo/zalo_corpus.json")
+        
+    corpus_paths = [
+        alqac23_corpus_path,
+        alqac22_corpus_path,
+        zalo_corpus_path
+    ]
+    
+    data = []
+    for corpus_path in corpus_paths:
+        data.extend(json.load(open(corpus_path)))
+        
     print("=======================")
     print("Start create doc refer.")
     documents = []
     doc_refers = []
     for law_article in tqdm(data):
-        law_id = law_article["law_id"]
+        law_id = law_article["id"]
         law_articles = law_article["articles"]
         
         for sub_article in law_articles:
-            article_id = sub_article["article_id"]
-            article_title = sub_article["title"]
+            article_id = sub_article["id"]
             article_text = sub_article["text"]
-            article_full = article_title + " " + article_text
-            doc_refers.append([law_id, article_id, article_full])
+            doc_refers.append([law_id, article_id, article_text])
     os.makedirs(args.save_path, exist_ok=True)
     with open(os.path.join(args.save_path,"doc_refers_saved.pkl"), "wb") as doc_refer_file:
         pickle.dump(doc_refers, doc_refer_file)
