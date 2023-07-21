2c8e5326c1d8c308028010a46490c1c201f7334f
k-dovan <khanhdovanit@gmail.com>
2023-07-21 01:50:27 +0000

    create question/article histogram, outliers filtered

---
+++ create_data_hist.py
@@ -0,0 +1,90 @@
+# ========================================================================================================
+# This file is for generating data distribution/histogram of questions/articles on specific data corpus.
+# ========================================================================================================
+
+import os
+import argparse
+import json
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw_data_dir", default="alqac23_data", type=str, help="path to raw corpus")
+    parser.add_argument("--corpus_name", default="alqac23", type=str, choices=["alqac23", "alqac22", "zalo"], help="corpus to evaluate")
+    args = parser.parse_args()
+
+    # save dir
+    save_dir = "generated_data/histograms"
+
+    # articles' paths
+    alqac23_corpus_path = os.path.join(args.raw_data_dir, "law.json")
+    alqac22_corpus_path = os.path.join(args.raw_data_dir, "additional_data/ALQAC_2022_training_data/law.json")
+    zalo_corpus_path = os.path.join(args.raw_data_dir, "additional_data/zalo/zalo_corpus.json")
+
+    # questions' paths
+    alqac23_corpus_path_train = os.path.join(args.raw_data_dir, "train.json")
+    alqac22_corpus_path_train = os.path.join(args.raw_data_dir, "additional_data/ALQAC_2022_training_data/question.json")
+    zalo_corpus_path_train = os.path.join(args.raw_data_dir, "additional_data/zalo/zalo_question.json")
+        
+    corpus_paths = {
+        "alqac23": alqac23_corpus_path,
+        "alqac22": alqac22_corpus_path,
+        "zalo": zalo_corpus_path
+    }
+    
+    train_corpus_paths = {
+        "alqac23": alqac23_corpus_path_train,
+        "alqac22": alqac22_corpus_path_train,
+        "zalo": zalo_corpus_path_train
+    }
+    
+    for corpus_name, data_path in corpus_paths.items():
+        data = json.load(open(data_path))
+            
+        data_lengths = []
+        for law_article in tqdm(data):
+            law_articles = law_article["articles"]
+            
+            outlier_thresh = 7000
+            for sub_article in law_articles:
+                data_len = len(sub_article["text"])
+                # from find outliers -> threshold of outliers
+                if data_len < outlier_thresh:
+                    data_lengths.append(data_len)
+
+        plt.hist(data_lengths, bins=20)
+        plt.savefig(f'{save_dir}/hist_{corpus_name}_articles.png')
+        plt.close()
+    
+    for corpus_name, train_path in train_corpus_paths.items():
+        data = json.load(open(train_path))
+            
+        data_lengths = []
+        for item in tqdm(data):
+            data_len = len(item["text"])
+            data_lengths.append(data_len)                    
+
+        plt.hist(data_lengths, bins=20)
+        plt.savefig(f'{save_dir}/hist_{corpus_name}_questions.png')
+        plt.close()
+                
+def find_outliers(corpus_name, data_path):
+    data = json.load(open(data_path))
+            
+    data_lengths = []
+    for law_article in tqdm(data):
+        law_articles = law_article["articles"]
+        
+        for sub_article in law_articles:
+            data_lengths.append(len(sub_article["text"]))
+    
+    np_arr = np.array(data_lengths)
+    top_value_idxs = np.argpartition(np_arr, -50)[-50:]
+    print (np_arr[top_value_idxs])
+    plt.hist(np_arr[top_value_idxs], bins=20)
+    plt.savefig(f'{save_dir}/hist_{corpus_name}_outliers.png')
+    plt.close()
+        
